38e9e74b5c3728c3657a8d69865937dec4aa6ba2
David Reagan <dmreagan@iu.edu>
2019-02-08 18:19:45 -0500

    Add rough color-graph

--- src/js/color_mapping_interaction.js
+++ src/js/color_mapping_interaction.js
@@ -263,6 +263,75 @@ async function loadSVG() {
       // store the current color
       selectedTarget.setAttribute('data-color', newColor);
     }
+
+    var yourVlSpec = {
+      $schema: 'https://vega.github.io/schema/vega-lite/v2.0.json',
+      description: 'A simple bar chart with embedded data.',
+      data: {
+        values: []
+      },
+      encoding: {
+        x: {field: 'length', type: 'ordinal', scale: { rangeStep: null }},
+        y: {field: 'radius', type: 'ordinal', scale: { rangeStep: null }},
+      },
+      layer: [{
+        mark: 'rect',
+        encoding: {
+          color: {
+            field: 'color',
+            scale: null
+          }
+        }
+      }, {
+        mark: 'circle',
+        encoding: {
+          color: {
+            condition: {
+              test: 'datum.selected',
+              value: 'cyan'
+            },
+            value: 'rgba(0, 0, 0, 0)'
+          }
+        }
+      }]
+    };
+
+    for (let r = 1; r <= numRows; r++) {
+
+      for (let l = 1; l <= numColumns; l++) {
+
+        const radius = colorMap[`${metal}_${medium}`].R[r][0];
+        const length = colorMap[`${metal}_${medium}`].R[0][l];
+
+        const red = colorMap[`${metal}_${medium}`].R[r][l];
+        const green = colorMap[`${metal}_${medium}`].G[r][l];
+        const blue = colorMap[`${metal}_${medium}`].B[r][l];
+
+        let selected = false;
+        if (r === radIndex && l === lenIndex) selected = true;
+
+        yourVlSpec.data.values.push({
+          length: length,
+          radius: radius,
+          color: `rgb(${red},${green},${blue})`,
+          selected: selected
+        });
+      }
+    }
+
+    console.log(yourVlSpec.data.values);
+
+
+
+
+
+    vegaEmbed('.bottom-ctl', yourVlSpec, {"actions": false});
+
+
+
+
+
+
   });
 
   input4.addEventListener('change', (event) => {
