123c3a39417338d128a093f71e75d222d0bee656
eddiewu5300 <bennice50@gmail.com>
2019-02-20 15:57:14 -0500

    fix the size but still blurry

--- src/js/color_spectrum.js
+++ src/js/color_spectrum.js
@@ -12,10 +12,8 @@ function colorgraph(metal, medium, numRows, numColumns, colorMap, radIndex = 1, 
   const yourVlSpec = {
     $schema: 'https://vega.github.io/schema/vega-lite/v2.0.json',
     description: 'A simple bar chart with embedded data.',
-    autosize: {
-      type: 'fit',
-      resize: 'true',
-    },
+    width: 200,
+    height: 200,
     data: {
       values: [],
     },
@@ -45,7 +43,8 @@ function colorgraph(metal, medium, numRows, numColumns, colorMap, radIndex = 1, 
       },
     }],
   };
-
+  const boxHeight = bottomCtl.clientHeight;
+  const boxWidth = bottomCtl.clientWidth;
   for (let r = 1; r <= numRows; r++) {
     for (let l = 1; l <= numColumns; l++) {
       const radius = colorMap[`${metal}_${medium}`].R[r][0];
@@ -66,6 +65,8 @@ function colorgraph(metal, medium, numRows, numColumns, colorMap, radIndex = 1, 
       });
     }
   }
+  yourVlSpec.height = boxHeight;
+  yourVlSpec.width = boxWidth;
   return yourVlSpec;
 }
 
@@ -97,13 +98,17 @@ selectedTarget = selectedTarget;
 
 // const numRows = colorMap[`${metal}_${medium}`].R.length - 1;
 // const numColumns = colorMap[`${metal}_${medium}`].R[0].length - 1;
+
 const goldGlassVec = colorgraph('Au', 'glass', colorMap.Au_glass.R.length - 1, colorMap.Au_glass.R[0].length - 1, colorMap);
 const silverGlassVec = colorgraph('Ag', 'glass', colorMap.Ag_glass.R.length - 1, colorMap.Ag_glass.R[0].length - 1, colorMap);
 const goldWaterVec = colorgraph('Au', 'water', colorMap.Au_water.R.length - 1, colorMap.Au_water.R[0].length - 1, colorMap);
 const silverWaterVec = colorgraph('Ag', 'water', colorMap.Ag_water.R.length - 1, colorMap.Ag_water.R[0].length - 1, colorMap);
 // selectedColor(5, 10, colorMap.Au_glass.R.length - 1, colorMap.Au_glass.R[0].length - 1, goldGlassVec);
-vegaEmbed('#color-spectrum', goldGlassVec, { actions: false });
-const marks = document.querySelector('.marks');
+let vegView = null;
+vegaEmbed('#color-spectrum', goldGlassVec, { actions: false }).then(result => vegView = result.view);
+vegView.width(500);
+vegView.run();
+// const marks = document.querySelector('.marks');
 // marks.setAttribute('style', 'width: 100%; height: 100%;');
 
 // vegaEmbed('#color-spectrum', silverGlassVec, { actions: false });
