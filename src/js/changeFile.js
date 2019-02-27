f37698a2404a15c9fa89b8270c8dbc239a5b79dc
eddiewu5300 <bennice50@gmail.com>
2019-02-27 11:11:18 -0500

    make it as a list buttons

--- src/js/changeFile.js
+++ src/js/changeFile.js
@@ -1,10 +1,12 @@
 
 
 /* eslint-disable no-console */
-const menu = document.querySelector('#menu');
+const menu = document.querySelector('#change-file-buttons');
 
 menu.addEventListener('click', (evt) => {
   const svgfile = evt.target.getAttribute('value');
-  document.querySelector('#my-stain-glass svg').remove();
-  loadSVG(svgfile);
+  if (svgfile) {
+    document.querySelector('#my-stain-glass svg').remove();
+    loadSVG(svgfile);
+  }
 });
