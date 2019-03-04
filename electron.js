cc9c5ed8bd129157c042b051c7674cdbc1025a21
eddiewu5300 <bennice50@gmail.com>
2019-03-04 17:30:28 -0500

    kiosk

--- electron.js
+++ electron.js
@@ -7,7 +7,7 @@ let mainWindow;
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ width: 800, height: 600, kiosk: true });
 
   // and load the index.html of the app.
   mainWindow.loadFile('src/index.html');
