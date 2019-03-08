ebf5ede2803368072250178ec5139b12a635a1dd
eddiewu5300 <bennice50@gmail.com>
2019-03-08 18:05:39 -0500

    change html

---
+++ src/js/idlePage.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-console */
+/* eslint-disable no-use-before-define */
+let timeoutID;
+
+function setup() {
+  this.addEventListener('mousemove', resetTimer, false);
+  this.addEventListener('mousedown', resetTimer, false);
+  this.addEventListener('keypress', resetTimer, false);
+  this.addEventListener('DOMMouseScroll', resetTimer, false);
+  this.addEventListener('mousewheel', resetTimer, false);
+  this.addEventListener('touchmove', resetTimer, false);
+  this.addEventListener('MSPointerMove', resetTimer, false);
+  console.log('djflkdjfoijeflkw');
+
+  startTimer();
+}
+setup();
+
+function startTimer() {
+  // wait 10 before calling goInactive
+  timeoutID = window.setTimeout(goInactive, 10000);
+}
+
+function resetTimer() {
+  window.clearTimeout(timeoutID);
+  console.log('active');
+
+  goActive();
+}
+
+function goInactive() {
+  window.location.href = 'idle.html';
+}
+
+function goActive() {
+  // do something
+  // window.location.href = 'index.html';
+  startTimer();
+}
